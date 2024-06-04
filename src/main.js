07b1227a4587fa1e5d41f415d369ec218f3e20db
ZiRazok <45107612+ZiRazok@users.noreply.github.com>
2024-06-05 00:40:33 +0300

    goit-js-hw-11
    Next day

--- src/main.js
+++ src/main.js
@@ -1,9 +1,16 @@
-// import iziToast from "izitoast";
-// import "izitoast/dist/css/iziToast.min.css";
+import iziToast from "izitoast";
+import "izitoast/dist/css/iziToast.min.css";
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+import pixabayApiFunction from "/js/pixabay-api";
+import renderElements from "/js/render-functions";
+
 
 const openSearchBtn = document.querySelector('.open-search');
 const mainForm = document.querySelector('.main-form');
 const mainInput = document.querySelector('.main-form-input');
+const mainList = document.querySelector('.main-list')
 
 let inputValue;
 
@@ -17,8 +24,33 @@ document.addEventListener('keyup', (event) => {
 
 mainForm.addEventListener("submit", (event) => {
     event.preventDefault();
-    if (!mainInput.value) { alert('Please enter text in search area'); return; };
+    if (!mainInput.value) {
+        iziToast.warning({
+            message: 'The input field cannot be empty!',
+            position: "topRight",
+            theme: 'dark',
+            backgroundColor: '#ef4040'
+        });
+        return;
+    };
     inputValue = mainInput.value;
     toggleIsOpen();
-    console.log(`Search: ${inputValue}`);
+
+    pixabayApiFunction(inputValue)
+        .then(post => {
+            if (post.hits.length == 0) {
+                iziToast.warning({
+                    message: 'Sorry, there are no images matching your search query. Please try again!',
+                    position: "topRight",
+                    theme: 'dark',
+                    backgroundColor: '#ef4040'
+                });
+                throw new Error('Cannot found');
+            };
+            console.log(post.hits);
+            mainList.insertAdjacentHTML("beforeend", renderElements(post.hits).join(""));
+            var lightbox = new SimpleLightbox('.main-list a', { captionsData: 'alt' });
+            lightbox.refresh();
+        })
+        .catch(error => console.log("ERROR:", error));
 });
